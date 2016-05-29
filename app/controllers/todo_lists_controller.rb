e00b960c6bf380ab95c0a5110c57ddc47e56c6e9
sathira <sathira.padukka@gmail.com>
2016-05-29 14:42:18 -0400

    Final Product

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -25,7 +25,7 @@ class TodoListsController < ApplicationController
   # POST /todo_lists.json
   def create
     @todo_list = TodoList.new(todo_list_params)
-
+ 
     respond_to do |format|
       if @todo_list.save
         format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
